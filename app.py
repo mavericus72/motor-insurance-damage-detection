31b385d8a5f47a46ff237c38005c8b8c071b2c65
Aniket Patil <73305339+mavericus72@users.noreply.github.com>
2026-04-09 09:36:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import torch
+from torchvision import transforms, models
+from torch import nn
+
+app = FastAPI()
+
+device = "cpu"  # Render uses CPU
+
+model = models.resnet18(weights=None)
+model.fc = nn.Linear(model.fc.in_features, 2)
+model.load_state_dict(torch.load("model.pth", map_location=device))
+model.to(device)
+model.eval()
+
+class_names = ['damage', 'no_damage']
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+@app.get("/")
+def home():
+    return {"message": "Damage Detection API"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = Image.open(file.file).convert("RGB")
+    image = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(image)
+        pred = torch.argmax(outputs, dim=1).item()
+
+    return {"prediction": class_names[pred]}
