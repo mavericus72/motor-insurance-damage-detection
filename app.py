2285087326541d6788285b9a1531af764fec6c47
Aniket Patil <73305339+mavericus72@users.noreply.github.com>
2026-04-10 09:59:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,40 @@ from PIL import Image
 import torch
 from torchvision import transforms, models
 from torch import nn
-import os
 
 app = FastAPI()
 
-device = "cpu"  # Render uses CPU
+# Downlaod Model
+MODEL_URL = "https://drive.google.com/uc?id=1Mbl_M8uvY_Riy-TPALQqA4k1XRy_WQrY"
+MODEL_PATH = "model.pth"
 
-model = models.resnet18(weights=None)
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model...")
+        response = requests.get(MODEL_URL, stream=True)
+        with open(MODEL_PATH, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print("Model downloaded.")
+    else:
+        print("Model already exists.")
+
+download_model()
+
+
+# Load model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = models.resnet18(pretrained=False)
 model.fc = nn.Linear(model.fc.in_features, 2)
-model_path = os.path.join(os.path.dirname(__file__), "model.pth")
-model.load_state_dict(torch.load(model_path, map_location=device))
+model.load_state_dict(torch.load("model.pth", map_location=device))
 model.to(device)
 model.eval()
 
+# Class names
 class_names = ['damage', 'no_damage']
 
+# Transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
@@ -25,7 +44,7 @@ transform = transforms.Compose([
 
 @app.get("/")
 def home():
-    return {"message": "Damage Detection API"}
+    return {"message": "Motor Insurance Damage Detection API"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
